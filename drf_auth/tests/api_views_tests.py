635dc00982c8ca9dce7ba41e2ba4d9c9ee053f1f
Martin Brochhaus <mbrochh@gmail.com>
2016-11-17 09:35:41 +0800

    Added tests for LoginAPIView

--- drf_auth/tests/api_views_tests.py
+++ drf_auth/tests/api_views_tests.py
@@ -18,7 +18,35 @@ class LoginAPIViewTestCase(TestCase):
     longMessage = True
 
     def test_view(self):
-        pass
+        user = mixer.blend('auth.User', is_active=False)
+        user.set_password('test')
+        user.save()
+        data = {}
+        req = APIRequestFactory().post('/', data=data)
+        resp = api_views.LoginAPIView().as_view()(req, version='v1')
+        self.assertEqual(resp.status_code, 400, msg=(
+            'Should return 400 if no data is given'))
+
+        data = {'email': user.email, 'password': 'wrong'}
+        req = APIRequestFactory().post('/', data=data)
+        resp = api_views.LoginAPIView().as_view()(req, version='v1')
+        self.assertEqual(resp.status_code, 400, msg=(
+            'Should return 400 if wrong password is given'))
+
+        data = {'email': user.email, 'password': 'test'}
+        req = APIRequestFactory().post('/', data=data)
+        resp = api_views.LoginAPIView().as_view()(req, version='v1')
+        self.assertEqual(resp.status_code, 400, msg=(
+            'Should return 400 if user is inactive'))
+
+        user.is_active = True
+        user.save()
+        data = {'email': user.email, 'password': 'test'}
+        req = APIRequestFactory().post('/', data=data)
+        req.session = self.client.session
+        resp = api_views.LoginAPIView().as_view()(req, version='v1')
+        self.assertEqual(resp.status_code, 200, msg=(
+            'Should return 200 if user has logged in'))
 
 
 class LogoutAPIViewTestCase(TestCase):
